7e5a748c99c63ddd9004eb630f2dbbaac9a1c346
TonuSH <tonutanushka66@gmail.com>
2024-02-27 09:26:49 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+function calculateGrade(){
+    let num = document.getElementById('grade').value;
+
+    let result;
+    if(num>=80 && num <=100){
+        result="A+";
+    }
+    else if(num>=70 && num<=79.99){
+        result="A";
+    }
+    else if(num>=60 && num<=69.99){
+        result="A-";
+    }
+    else if(num>=50 && num<=59.99){
+        result="B";
+    }
+    else if(num>=40 && num<=49.99){
+        result="C";
+    }
+    else if(num>=33 && num<=39.99){
+        result="D";
+    }
+    else if(num>=0 && num<=33.99){
+        result="F";
+    }
+    else{
+        result="Invalid";
+    }
+    document.getElementById('result').innerText = ` Your result is ${result} `;
+}
